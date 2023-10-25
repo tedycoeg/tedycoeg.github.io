b43b25a227401cf2369339d08b08545136c7f591
tedycoeg <tedycoeg5000@gmail.com>
2023-10-25 09:45:17 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$("#login-button").click(function (event) {
+ 
+
+  var userName = document.getElementById("userName").value;
+  var pwd = document.getElementById("pwd").value;
+  console.log(userName);
+  console.log(pwd);
+  if (
+    (userName == "Nana Yuniar" ||
+      userName == "nana yuniar" ||
+      userName == "Nana yuniar") &&
+    pwd == "251001"
+  ) {
+    // username and password
+    event.preventDefault();
+    $("form").fadeOut(500);
+    $(".wrapper").addClass("form-success");
+    setTimeout(function () {
+      location.href = "BirthdayCake.html";
+    }, 2000);
+  } else {
+    alert("Wrong Password");
+  }
+});
